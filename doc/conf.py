554badbc46ea8cfadd3f575b20fbb1c2f3be3cec
Parth Sanepara <parthsanepara@gmail.com>
2025-05-08 23:08:12 +0530

    PSLB-18 Update project documentation and versioning for PSLabel

--- doc/conf.py
+++ doc/conf.py
@@ -6,15 +6,21 @@
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
-project = 'NCS Example Application'
-copyright = '2024, The Zephyr Community'
-author = 'The Zephyr Community'
-release = '1.0.0'
+project = 'PSLabel'
+copyright = '2025, HPRS'
+author = 'HPRS Team'
+release = '0.1.0'
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = []
+extensions = [
+    'sphinx.ext.autodoc',        # Automatically document code
+    'sphinx.ext.napoleon',       # Support for Google/NumPy docstrings
+    'sphinx.ext.viewcode',       # Add links to highlighted source code
+    'sphinx.ext.todo',           # Support for TODOs
+    'sphinx.ext.githubpages',    # Publish to GitHub Pages
+]
 
 templates_path = ['_templates']
 exclude_patterns = ['_build_sphinx', 'Thumbs.db', '.DS_Store']
--- doc/index.rst
+++ doc/index.rst
@@ -1,7 +1,20 @@
 Welcome to NCS Example Application's documentation!
 ===================================================
 
-This is a simple Sphinx documentation setup for ``ncs-example-application``.
+**PSLabel** is an open-source platform for creating scalable, ultra-low-power eInk label systems. This documentation provides an overview of the project, its architecture, and guides for developers.
+
+Contents
+========
+
+.. toctree::
+   :maxdepth: 2
+   :caption: Contents:
+
+   introduction
+   installation
+   usage
+   api_reference
+   contributing
 
 Indices and tables
 ==================
